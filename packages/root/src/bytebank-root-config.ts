1d4f4b27c518297663c3a4dcd92b4d9cd2d536ff
luizamiotto <luiza.miotto@ludfor.com.br>
2025-10-09 00:07:08 -0300

    Ajustando renderização dos microfrontends

--- packages/root/src/bytebank-root-config.ts
+++ packages/root/src/bytebank-root-config.ts
@@ -4,41 +4,49 @@ import "@bytebank/styles";
 
 export { ResponsiveProvider, useResponsive };
 
+const getEl = (id: string) => () => document.getElementById(id);
+
 registerApplication({
   name: "@bytebank/header",
   app: () => System.import<LifeCycles>("//localhost:8500/bytebank-header.js"),
-  activeWhen: (location) => location.pathname === '/',
+  activeWhen: ["/"],
+  customProps: { domElement: getEl('mf-header')() },
 });
 
 registerApplication({
   name: "@bytebank/sidebar",
   app: () => System.import<LifeCycles>("//localhost:8501/bytebank-sidebar.js"),
-  activeWhen: (location) => location.pathname === '/',
+  activeWhen: ["/"],
+  customProps: { domElement: getEl('mf-sidebar')() },
 });
 
-// registerApplication({
-//   name: "@bytebank/welcome",
-//   app: () => System.import<LifeCycles>("//localhost:8502/bytebank-welcome.js"),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@bytebank/welcome",
+  app: () => System.import<LifeCycles>("//localhost:8502/bytebank-welcome.js"),
+  activeWhen: ["/"],
+  customProps: { domElement: getEl('mf-welcome')() },
+});
 
-// registerApplication({
-//   name: "@bytebank/statement",
-//   app: () => System.import<LifeCycles>("//localhost:8503/bytebank-statement.js"),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@bytebank/statement",
+  app: () => System.import<LifeCycles>("//localhost:8503/bytebank-statement.js"),
+  activeWhen: ["/"],
+  customProps: { domElement: getEl('mf-statement')() },
+});
 
-// registerApplication({
-//   name: "@bytebank/transaction",
-//   app: () => System.import<LifeCycles>("//localhost:8504/bytebank-transaction.js"),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@bytebank/transaction",
+  app: () => System.import<LifeCycles>("//localhost:8504/bytebank-transaction.js"),
+  activeWhen: ["/"],
+  customProps: { domElement: getEl('mf-transaction')() },
+});
 
-// registerApplication({
-//   name: "@bytebank/investments",
-//   app: () => System.import<LifeCycles>("//localhost:8505/bytebank-investments.js"),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@bytebank/investments",
+  app: () => System.import<LifeCycles>("//localhost:8505/bytebank-investments.js"),
+  activeWhen: ["/"],
+  customProps: { domElement: getEl('mf-investments')() },
+});
 
 start({
   urlRerouteOnly: true,
